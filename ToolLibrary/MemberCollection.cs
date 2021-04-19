214fd83307613bb8ffad8a802fb6fe66dbf968c4
CHENG-LIANG1 <madfoxfoxy@gmail.com>
2021-04-19 17:35:43 +1000

    Added necessary classes

---
+++ ToolLibrary/MemberCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolLibrary
+{
+    class MemberCollection : iMemberCollection
+    {
+        public int Number => throw new NotImplementedException();
+
+        public void add(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool search(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public iMember[] toArray()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToolLibrary/ToolLibrary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolLibrary
+{
+    class ToolLibrary : iToolLibrarySystem
+    {
+        public void add(iTool tool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(iTool tool, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void borrowTool(iMember member, iTool tool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iTool tool)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iTool tool, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void display(string phoneNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void displayTools(string toolType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void displayTopTHree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] listTools(iMember member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void returnTool(iMember member, iTool tool)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
